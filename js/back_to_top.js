408033f2c13cbb382480b3c61b9f12ec18f31af9
HerobrainGamer <herobraingaming@gmail.com>
2020-06-05 17:43:18 -0700

    code
    added an about part to the hover menus.
    added who the site was coded by.

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -1,4 +1,3 @@
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -9,8 +8,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
