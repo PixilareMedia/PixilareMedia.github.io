cb002ba5a876cc34677706e3ea9102ee98541df5
HerobrainGamer <herobraingaming@gmail.com>
2020-12-18 19:30:26 -0800

    code
    - redesigned the site
    - removed dead projects
    - removed members who have left
    - split css & js into multiple files
    - reorganised images

---
+++ assets/js/hollow-pvp.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", hpHiddenBlocks);
+
+function hpHiddenBlocks() {
+  document.getElementById("hp7dlw").style.display = "none";
+  document.getElementById("hp8dlw").style.display = "none";
+  document.getElementById("hp9dlw").style.display = "none";
+  document.getElementById("hp10dlw").style.display = "none";
+  document.getElementById("hp11dlw").style.display = "none";
+  document.getElementById("hp12dlw").style.display = "none";
+  document.getElementById("hp13dlw").style.display = "none";
+  document.getElementById("hp14dlw").style.display = "none";
+  document.getElementById("hp15dlw").style.display = "none";
+  document.getElementById("hp16dlw").style.display = "none";
+}
+
+function hp16dlf() {
+    var x = document.getElementById("hp16dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp15dlf() {
+    var x = document.getElementById("hp15dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp14dlf() {
+    var x = document.getElementById("hp14dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp13dlf() {
+    var x = document.getElementById("hp13dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp12dlf() {
+    var x = document.getElementById("hp12dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp11dlf() {
+    var x = document.getElementById("hp11dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp10dlf() {
+    var x = document.getElementById("hp10dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp9dlf() {
+    var x = document.getElementById("hp9dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp8dlf() {
+    var x = document.getElementById("hp8dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function hp7dlf() {
+    var x = document.getElementById("hp7dlw");
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
